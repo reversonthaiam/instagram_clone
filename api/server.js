6749d30110cecb9a2fd34037e01328affd5b9137
reversonthaiam <70376757+reversonthaiam@users.noreply.github.com>
2020-12-21 15:10:33 -0300

    api restfull

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     mongodb = require('mongodb');
-    objectId = require('mongodb').ObjectId;
+objectId = require('mongodb').ObjectId;
 
 var app = express();
 
 // body-parser
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = 8080;
@@ -21,9 +21,9 @@ app.listen(port);
 
 console.log('Servidor HTTP esta escutando na porta ' + port);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 
-    var resposta = {msg: 'olá'}
+    var resposta = { msg: 'olá' }
     res.send(resposta);
 });
 
@@ -31,18 +31,18 @@ app.get('/', function(req, res){
 
 //create post
 
-app.post('/api', function(req, res){
+app.post('/api', function (req, res) {
 
     var dados = req.body;
 
-    db.open( function(err, mongoclient){
-        mongoclient.collection('postagens', function(err, collection){
-            collection.insert(dados, function(err, records){
-                if(err){
-                    res.json({'status' : 'erro'});
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.insert(dados, function (err, records) {
+                if (err) {
+                    res.json({ 'status': 'erro' });
 
-                } else{
-                    res.json({'status' : 'inclusao realizada com sucesso'});
+                } else {
+                    res.json({ 'status': 'inclusao realizada com sucesso' });
 
                 }
                 mongoclient.close();
@@ -53,16 +53,16 @@ app.post('/api', function(req, res){
 })
 
 //get reader
-app.get('/api', function(req, res){
+app.get('/api', function (req, res) {
 
-    db.open( function(err, mongoclient){
-        mongoclient.collection('postagens', function(err, collection){
-            collection.find().toArray( function(err, results){
-                
-                if(err){
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.find().toArray(function (err, results) {
+
+                if (err) {
                     res.json(err);
 
-                } else{
+                } else {
                     res.json(results);
                 }
 
@@ -77,16 +77,16 @@ app.get('/api', function(req, res){
 
 //get by id
 
-app.get('/api/:id', function(req, res){
+app.get('/api/:id', function (req, res) {
 
-    db.open( function(err, mongoclient){
-        mongoclient.collection('postagens', function(err, collection){
-            collection.find(objectId(req.params.id)).toArray( function(err, results){
-                
-                if(err){
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.find(objectId(req.params.id)).toArray(function (err, results) {
+
+                if (err) {
                     res.json(err);
 
-                } else{
+                } else {
                     res.json(results);
                 }
 
@@ -94,6 +94,57 @@ app.get('/api/:id', function(req, res){
 
                 mongoclient.close();
             })
+        })
+    })
+
+})
+
+//put para fazer update
+
+app.put('/api/:id', function (req, res) {
+
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.update(
+
+                { _id: objectId(req.params.id) },
+                { $set: { titulo: req.body.titulo } },
+                {},
+                function (err, records) {
+
+                    if (err) {
+                        res.json(err);
+                    } else {
+                        res.json(records);
+                    }
+
+                    mongoclient.close();
+                })
+        })
+    })
+
+})
+
+
+//delete para pagar documentos
+
+app.delete('/api/:id', function (req, res) {
+
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.remove({ _id : objectId(req.params.id)}, function(err, records){
+                if(err){
+                    res.json(err);
+
+                } else{
+                    res.json(records)
+                }
+
+                mongoclient.close();
+            })
+
+                
+                
         })
     })
 
