e25dfaecab54f2716a30b37e2780ec7f17f7d5a5
reversonthaiam <70376757+reversonthaiam@users.noreply.github.com>
2020-12-23 16:41:58 -0300

    api instagram restfull completa

--- api/server.js
+++ api/server.js
@@ -166,10 +166,20 @@ app.put('/api/:id', function (req, res) {
 
 // DELETE by ID(delete)
 app.delete('/api/:id', function (req, res) {
+
+
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
-            collection.remove(
-                { _id: objectId(req.params.id) },
+            collection.update(
+                {},
+                {
+                    $pull: {
+
+                        comentarios: { id_comentario: objectId(req.params.id) }
+                    }
+                },
+                { multi: true },
+
                 function (err, records) {
                     if (err) {
                         res.json(err);
